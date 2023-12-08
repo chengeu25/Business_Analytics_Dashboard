58bbec15c5d20f36d96fa8027983223950afe518
chengeu25 <118132703+chengeu25@users.noreply.github.com>
2023-12-08 02:21:51 +0000

    Added Sales Trend Visualization

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ from sales_data import SalesData
 
 data_set = input("Enter file path: ")
 
+
 def main_menu(input_sales_data):
   print("Welcome to Business Analytics Dashboard")
   print("1. View Sales Report")
   print("2. View Sales by Region")
+  print("3. View Sales Trend Over Time")
   choice = input("Enter you choice: ")
 
   if choice == "1":
     total_sales = input_sales_data.total_sales()
-    print(total_sales)
+    print("total sales is: " + str(total_sales) + ".\n\n")
   elif choice == "2":
     region_sales = input_sales_data.sales_by_region()
-    print(region_sales)
+    print("Sales by region: \n" + str(region_sales) + "\n\n")
+  elif choice == "3":
+    sales_data.visualize_sales_trends()
   else:
     print("invalid choice: please try again")
+  choice = input("Enter you choice: ")
 
 
 if __name__ == "__main__":
--- sales_data.py
+++ sales_data.py
@@ -1,13 +1,22 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 class SalesData:
 
   def __init__(self, file_path):
     self.data = pd.read_csv(file_path, encoding='latin1')
-
+    self.data['ORDERDATE'] = pd.to_datetime(self.data['ORDERDATE'])
+    
   def total_sales(self):
-    return self.data["PRICEEACH"].sum()
+    return self.data["PRICEEACH"].sum() #mutiply by number of sales for each item
 
   def sales_by_region(self):
     return self.data.groupby("STATE")["PRICEEACH"].sum()
+
+  def visualize_sales_trends(self):
+    monthly_sales = self.data.resample('M', on = 'ORDERDATE')['SALES'].sum()
+    plt.plot(monthly_sales)
+    plt.xlabel("Date")
+    plt.ylabel("Sales Amount ($)")
+    plt.title("Sales Trend Over Time")
+    plt.show()

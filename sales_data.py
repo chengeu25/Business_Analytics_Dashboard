7847216f2f36f751a5aeed6920e5728a5a5409fd
chengeu25 <118132703+chengeu25@users.noreply.github.com>
2023-12-09 00:13:22 +0000

    Update

--- sales_data.py
+++ sales_data.py
@@ -12,6 +12,11 @@ class SalesData:  #public class SalesData {}
     total_sales_generated = self.data["SALES"].sum()
     return total_sales_generated
 
+  # def product_sales(self):
+  #   unique_product_lines = self.data['PRODUCTLINE'].unique()
+  #   for product_line in unique_product_lines:
+  #     total_product_sales = self.data.groupby(str(product_line))["SALES"].sum()
+
   def sales_by_country(self):
     sales_by_country_generated = self.data.groupby("COUNTRY")["SALES"].sum()
     return sales_by_country_generated
@@ -20,13 +25,26 @@ class SalesData:  #public class SalesData {}
     sales_by_state_generated = self.data.groupby("STATE")["SALES"].sum()
     return sales_by_state_generated
 
-  def visualize_sales_trends(self):
+  def visualize_month_trends(self):
     monthly_sales = self.data.resample('M', on='ORDERDATE')['SALES'].sum()
     plt.plot(monthly_sales)
     plt.xlabel("Date")
     plt.ylabel("Sales Amount ($)")
-    plt.title("Sales Trend Over Time")
+    plt.title("Monthly Sales Trend")
     plt.show()
 
-#PRODUCTLINE
-#
+  def visualize_yearly_trends(self):
+    yearly_sales = self.data.resample('Y', on='ORDERDATE')['SALES'].sum()
+    plt.plot(yearly_sales)
+    plt.xlabel("Date")
+    plt.ylabel("Sales Amount ($)")
+    plt.title("Yearly Sales Trend")
+    plt.show()
+
+  def visualize_quarterly_trends(self):
+    quarterly_sales = self.data.resample('Q', on='ORDERDATE')['SALES'].sum()
+    plt.plot(quarterly_sales)
+    plt.xlabel("Date")
+    plt.ylabel("Sales Amount ($)")
+    plt.title("Quarterly Sales Trend")
+    plt.show()
